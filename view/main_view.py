7477e6f61c82165480566979e606087dd34207d2
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2020-11-25 09:06:41 +0200

    added bullet points options to views

--- view/main_view.py
+++ view/main_view.py
@@ -6,10 +6,13 @@ Main view module
 class View:
 
     @staticmethod
-    def show_items_menu(list_items: list, title: str):
+    def show_items_menu(list_items: list, title: str, display_bullet_points=False):
         print(20 * "+")
         print(title.upper())
         print(20 * "+")
         for item_index in range(1, len(list_items)+1):
-            print(f"[{item_index}] {list_items[item_index-1]}")
+            if display_bullet_points is True:
+                print("{bullet} {item}".format(bullet="*", item=list_items[item_index-1]))
+            else:
+                print("[{index}] {item}".format(index=item_index, item=list_items[item_index-1]))
         print(20 * "+")
