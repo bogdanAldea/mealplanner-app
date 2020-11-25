c2fcc27b803d1340937676e21b33d2a3836b2276
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2020-11-25 12:02:46 +0200

    view stock items function created

--- view/main_view.py
+++ view/main_view.py
@@ -16,3 +16,12 @@ class View:
             else:
                 print("[{index}] {item}".format(index=item_index, item=list_items[item_index-1]))
         print(20 * "+")
+
+    @staticmethod
+    def read_stock_data(stock_data_list: dict):
+
+        for main_key, main_value in stock_data_list.items():
+            print("\n" + 20 * "+")
+            print(main_key.upper())
+            for sub_key, sub_value in main_value.items():
+                print(sub_key + " -> " + str(sub_value))
