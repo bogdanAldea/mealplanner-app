77abea5553ac5d70f8db6cdf524a31100daa87e7
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-02-08 11:07:37 +0200

    modified method that updates compnent database

--- components/component_manager.py
+++ components/component_manager.py
@@ -14,17 +14,12 @@ class ComponentManager:
     def read_database_file(self):
         return json_db.read_file(file_path=self.database)
 
-    def save(self, data_to_save: dict) -> dict:
-        new_data: dict = json_db.save_data(new_data=data_to_save)
-        self.loaded_data.update(new_data)
-        return self.loaded_data
+    def update_new_ingredient(self):
+        json_db.save_data(file=self.database, new_data=self.loaded_data)
 
     def load(self) -> dict:
         loaded_data: dict = json_db.load_data(file_data=self.connection)
         return loaded_data
-
-    def update(self, data_to_update: dict) -> None:
-        json_db.update_file(file_path=self.database, new_data=data_to_update)
 
     def select_component_values(self, request: int) -> list:
         installed_components: list = list(self.loaded_data.keys())
