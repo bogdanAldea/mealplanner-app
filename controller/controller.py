8578e8fd65adc0a8d47bc083d3eec99be9b464dc
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-02-08 11:08:44 +0200

    updated module with code documentation

--- controller/controller.py
+++ controller/controller.py
@@ -2,15 +2,102 @@
 """
 import models
 import views
-import os
 
 
 class Controller:
-    EXCEPTIONS = models.exceptions
+
+    exceptions = models.exceptions
 
     def __init__(self, VIEWS, MODELS):
         self.models: models.models = MODELS
         self.views: views = VIEWS
+
+    """
+    HELPER METHODS FOR HANDLING ERRORS    
+    """
+
+    def validate_recipe_name(self):
+
+        # list of all recipes names already stored
+        inventory = [recipe.get_name for recipe in self.models.recipe_creator.recipe_objects]
+
+        # filter requested name and return if not found in inventory
+        while True:
+            try:
+                requested_recipe_name = input("Set name >>> ")
+                valid_name = self.models.validate_recipe_name(name_request=requested_recipe_name, recipe_inventory=inventory)
+                return valid_name
+            except Controller.exceptions.RecipeIsStored as e:
+                self.views.display_error(e)
+
+    def validate_save_request(self):
+        options = ["y", "Y", "n", "N"]
+        while True:
+            try:
+                save_request = input("\nWant to save your data? >>> ")
+                valid_request = self.models.validate_save_request(request=save_request, options=options)
+                return valid_request
+            except Controller.exceptions.InvalidSaveRequest as e:
+                self.views.display_error(e)
+
+    def validate_menu_request(self, request, menu_options):
+        while True:
+            try:
+                valid_menu_request = self.models.validate_menu_request(request, menu_options)
+                return valid_menu_request, True
+            except Controller.exceptions.MenuOptionInvalid as e:
+                self.views.display_error(e)
+                return None, False
+
+    def validate_ingredient_name(self, component_values):
+        try:
+            requested_ingredient_name = input("Set ingredient name >>> ")
+            valid_request = self.models.validate_ingredient_name(name_request=requested_ingredient_name, stored_values=component_values)
+            return valid_request, True
+        except Controller.exceptions.IngredientIsStored as e:
+            self.views.display_error(e)
+            return None, False
+
+    """
+    DATA SAVING METHODS FOR EACH MUTABLE OBJECT
+    """
+
+    def save_recipe(self, recipe_to_save):
+
+        # refresh screen & render save screen
+        save_screen = self.views.SaveRecipe(recipe_to_save)
+        save_screen.render_screen()
+
+        while True:
+            try:
+                request = self.validate_save_request()
+
+                if request in ["y", "Y"]:
+                    self.models.save_recipe(recipe_to_save)
+                    save_screen.display_saving_status()
+
+                elif request in ["n", "N"]:
+                    break
+
+                return request
+
+            except Controller.exceptions.InvalidSaveRequest as e:
+                self.views.display_error(e)
+                continue
+
+    def save_component(self):
+        ...
+
+    def save_ingredient(self):
+        ...
+
+    def save_inventory(self):
+        ...
+
+    """
+    MAIN METHODS: CONTROLLER ACCEPTS REQUESTS FROM USERS THROUGH VIEWS CLASS
+    AND RETRIEVES / UPDATES DATA THROUGH MODELS CLASS
+    """
 
     def component_selector(self, components_list, recipe_status):
 
@@ -20,65 +107,55 @@ class Controller:
 
         # get user request for component selection
         while True:
-            component_selection_request = input("\nSelect component >>> ")
+            selector_request = input("\nSelect component >>> ")
 
-            # check if request is value of exit/back
-            if component_selection_request in ["q", "Q"]:
-                return None, component_selection_request
+            # check if request is a value of exit/break
+            if selector_request in ["q", "Q"]:
+                return None, selector_request
 
             else:
-
-                # check if request is a valid choice from range of len(components_list)
-                if int(component_selection_request) not in range(1, len(components_list)+1):
-                    continue
+                valid_request, flag = self.validate_menu_request(request=int(selector_request), menu_options=components_list)
+                if flag:
+                    ingredient_values = self.models.component_creator.select_component_values(request=int(valid_request))
+                    return ingredient_values, selector_request
 
                 else:
-                    ingredient_values = self.models.component_creator.select_component_values(request=int(component_selection_request))
-                    return ingredient_values, component_selection_request
+                    continue
 
     def ingredient_selector(self, ingredients_list, recipe_status):
 
         # print menu to the screen via views class
-        ingredient_screen = self.views.IngredientSelector(ingredient_menu_list=ingredients_list, recipe_status=recipe_status)
+        ingredient_screen = self.views.IngredientSelector(ingredient_menu_list=ingredients_list,recipe_status=recipe_status)
         ingredient_screen.render_screen()
 
-        # get user request for ingredient selection
+        # check if request is value of exit/back
         while True:
-            ingredient_selection_request = input("\nSelect ingredient >>> ")
 
-            # check if request is value of exit/back
-            if ingredient_selection_request in ["q", "Q"]:
-                return None, None, ingredient_selection_request
+            selector_request = input("\nSelect ingredient >>> ")
+
+            # check if request is a value of exit/back
+            if selector_request in ["q", "Q"]:
+                return None, None, selector_request
 
             else:
-
-                # check if request is a valid choice from range of len(ingredients_list)
-                if int(ingredient_selection_request) not in range(1, len(ingredients_list)+1):
-                    continue
-
-                else:
-                    selected_ingredient = self.models.component_creator.select_from_values(request=int(ingredient_selection_request),
+                valid_request, flag = self.validate_menu_request(request=int(selector_request), menu_options=ingredients_list)
+                if flag:
+                    selected_ingredient = self.models.component_creator.select_from_values(request=int(selector_request),
                                                                                            values=ingredients_list)
                     selected_quantity = int(input("Quantity >>> "))
-                    return selected_ingredient, selected_quantity, ingredient_selection_request
+                    return selected_ingredient, selected_quantity, selector_request
 
-    def validate_recipe_name(self):
+                else:
+                    continue
 
-        # list of all recipe names already stored
-        inventory = [recipe.get_name for recipe in self.models.recipe_creator.recipe_objects]
-
-        # filter requested name and return if not found in inventory
-        while True:
-            try:
-                recipe_name = input("Set name >>> ")
-                valid_name = self.models.validate_recipe_name(name_request=recipe_name, recipe_inventory=inventory)
-                return valid_name
-            except Controller.EXCEPTIONS.RecipeIsStored as e:
-                print(e.args[0])
-
+    """
+    METHOD IMPLEMENTS FUNCTIONALITY FOR CREATING A BRAND NEW RECIPE
+    - SELECTS COMPONENT
+    - SELECTS INGREDIENTS & QUANTITIES
+    """
     def create_recipe(self):
 
-        # refresh and clear screen
+        # refresh and clear screen / render create recipe screen
         screen = self.views.Create_Recipe()
         screen.render_screen()
 
@@ -103,54 +180,76 @@ class Controller:
         while True:
 
             # unpack returned values from component_selector method
-            selected_component, menu_request = self.component_selector(components_list=components, recipe_status=recipe_status)
+            selected_component, menu_request = self.component_selector(components_list=components,
+                                                                       recipe_status=recipe_status)
 
             # check if requested option is not a value for exit/back
             if menu_request not in ["q", "Q"]:
 
                 # unpack values from ingredient_selector method
-                ingredient, quantity, menu_request = self.ingredient_selector(ingredients_list=selected_component, recipe_status=recipe_status)
+                ingredient, quantity, menu_request = self.ingredient_selector(ingredients_list=selected_component,
+                                                                              recipe_status=recipe_status)
 
                 # check if ingredient request is not a value of exit/back
                 if menu_request not in ["q", "Q"]:
+
+                    # assemble ingredient-quantity pair
                     recipe_ingredients[ingredient] = quantity
 
             else:
                 break
 
-        # create recipe object with selected items
+        # create recipe object using selected items & recipe blueprint
         recipe_object = recipe_blueprint(name=recipe_name, ingredients=recipe_ingredients)
-        return recipe_object
+        return menu_request, recipe_object
 
-    def save_recipe(self, recipe_to_save):
+    """
+    METHOD IMPLEMENTS FUNCTIONALITY FOR CREATING A NEW INGREDIENT
+    - CREATES INGREDIENT & SORTS IT BASED ON COMPONENT SELECTED
+    """
+    def create_ingredient(self, request, stored_ingredients):
 
-        # refresh and clear screen / render save screen
-        save_screen = self.views.SaveRecipe(recipe_to_save)
-        save_screen.render_screen()
+        # refresh screen/render ingredient creation screen
+        create_ingredient_screen = self.views.NewIngredient()
+        create_ingredient_screen.render_screen()
 
-        # prompt user to save /not save recipe
         while True:
-            try:
-                request = self.validate_save_request()
+            # set ingredient name
+            ingredient_name, flag = self.validate_ingredient_name(component_values=stored_ingredients)
 
-                if request in ["y", "Y"]:
+            if flag:
+                # create ingredient object with component creator matrix
+                ingredient_object = self.models.component_creator.create_component(component_name=ingredient_name, request=request)
+                return ingredient_object
 
-                    recipe = recipe_to_save.__dict__
-                    self.models.save_recipe(recipe)
-                    save_screen.display_saving_status(status="Recipe is being saved...")
-                    break
-
-                elif request in ["n", "N"]:
-                    print("recipe not saved...")
-                    break
-            except Controller.EXCEPTIONS.InvalidSaveRequest as e:
-                save_screen.display_error(exception_error=e)
+            else:
                 continue
 
-    @staticmethod
-    def validate_save_request():
-        save_request = input("\nWant to save recipe? >>> ")
-        if save_request in ["y", "Y"] or save_request in ['n', "N"]:
-            return save_request
-        else:
-            raise Controller.EXCEPTIONS.InvalidSaveRequest("Option invalid. The valid options are Y/y or Q/q.")
+    """
+    METHOD ALLOWS FOR CREATING & ADDING NEW INGREDIENTS
+    - SELECTS COMPONENT TO ADD INGREDIENTS TO
+    - SORTS NEWLY ADDED INGREDIENT TO ITS SELECTED COMPONENT
+    """
+    def add_new_ingredient(self):
+
+        # components list for views to display as menu
+        components = self.models.component_creator.installed_components.keys()
+
+        while True:
+            # select component to add ingredient to
+            component_values, component_selector = self.component_selector(components_list=components,
+                                                                           recipe_status=None)
+
+            # check if component selector is not a value for quit.back
+            if component_selector not in ["q", "Q"]:
+
+                # create new ingredient
+                ingredient_object = self.create_ingredient(request=int(component_selector),
+                                                           stored_ingredients=component_values)
+
+                # sort newly created ingredient to its specific component
+                self.models.component_creator.sort_component(new_component=ingredient_object)
+                return component_selector
+
+            else:
+                break
