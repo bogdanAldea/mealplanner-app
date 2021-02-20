9e8cb708000b72d1d6a101ad3faf589245dc878d
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-02-20 14:19:41 +0200

    added doctring to classes

--- components/object_sets.py
+++ components/object_sets.py
@@ -1,17 +1,21 @@
 """
-MODULE DEFINES WHAT A COMPONENT SET AND AN INGREDIENT SET ARE
-ALLOWS FOR EASIER MANIPULATION OF MORE COMPLEX OBJECTS
+MODULE DEFINES CLASSES THAT REPRESENTS A PAIR / SET OF:
+- COMPONENTS -> DESCRIBED BY TYPE AND AN COLLECTION AN VALUES
+- INGREDIENTS -> DESCRIBED BY A NAME AND A GIVEN QUANTITY
 """
 
 
-class ComponentSet:
+class ComponentsSet:
+    """
+    Class represents an object set defined by its given type and its specific values.
+    Ex: Type: Fruit, Items: list of fruit names.
+    """
 
     def __init__(self, obj_type: str, obj_items: list):
         """
-        CLASS DESCRIBES AN OBJECT DEFINED BY A TYPE (MEAT, VEGETABLE ETC...) ALONG WITH ITS VALUES
-        :param obj_type: str determined by the type of its basic component
-        :param obj_items: list of values specific for their component type
-                          (ex: for the type "Vegetable" -> values = ["carrot", "onion" etc])
+        Constructor of component set class.
+        :param obj_type: str
+        :param obj_items: list
         """
         self.obj_type: str = obj_type
         self.obj_items: list = obj_items
@@ -20,27 +24,53 @@ class ComponentSet:
         return getattr(self, item)
 
     @property
-    def get_type(self) -> str:
+    def get_obj_type(self) -> str:
         return self.obj_type
 
     @property
-    def get_items(self) -> list:
+    def get_obj_items(self) -> list:
         return self.obj_items
+
+    def export_dict(self) -> dict:
+        """
+        Method returns a dict version of the object used for json serialization.
+        :return: dict
+        """
+        return self.__dict__
 
 
 class IngredientSet:
+    """
+    Class represents an object set defined by a name and a quantity.
+    Ex: Name: Apple, Quantity: 1
+    """
 
-    def __init__(self, obj_name: str, obj_quantity: int):
+    def __init__(self, name: str, quantity: int):
         """
-        CLASS REPRESENT AN UNIQUE SET/PAIR OF INGREDIENTS AND THEIR QUANTITIES
-        :param obj_name: str given by user/selection
-        :param obj_quantity: number >= 0
+        Constructor of ingredient set class.
+        :param name: str
+        :param quantity: int
         """
-        self.obj_name: str = obj_name
-        self.obj_quantity: int = obj_quantity
+        self.name = name
+        self.quantity = quantity
 
     def __getitem__(self, item):
         return getattr(self, item)
 
-    def set_quantity(self, new_value: int) -> None:
-        self.obj_quantity = new_value
+    @property
+    def get_name(self):
+        return self.name
+
+    @property
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, value) -> None:
+        self.quantity = value
+
+    def export_dict(self) -> dict:
+        """
+        Method returns a dict version of the objects for json serialization.
+        :return: dict
+        """
+        return self.__dict__
