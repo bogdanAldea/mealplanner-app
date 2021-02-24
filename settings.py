d9aa307ebb6c855057e1e29c8745c044d1fef52c
Bogdan Aldea <bogdan.aldea0623@gmail.com>
2021-02-25 00:30:17 +0200

    renamed ingredients file name

--- settings.py
+++ settings.py
@@ -1,19 +1,26 @@
 import pathlib
-import components as components
+import components.ingredients as ingredients
 
 
+# SETTING BASE DIRECTORY PATH
 BASEDIR = pathlib.Path(__file__).parents[0]
 
-RECIPES_JSON_PATH = BASEDIR / "recipes.json"
-COMPONENTS_JSON_PATH = BASEDIR / "components.json"
+# SETTING RECIPES FILE PATH FROM BASEDIR
+RECIPE_JSON_PATH = BASEDIR / 'recipes.json'
 
-INSTALLED = \
-    {
-        "Meat": components.component.Meat,
-        "Dairy": components.component.Dairy,
-        "Vegetable": components.component.Vegetable,
-        "Flour": components.component.Flour,
-        "Fruit": components.component.Fruit,
-        "Other": components.component.Other,
-        "Custom": components.component.Custom
-    }
+# SETTING INVENTORY FILE PATH WITH FROM BASEDIR
+INGREDIENTS_JSON_PATH = BASEDIR / 'ingredients.json'
+
+# SETTING COMPONENTS FILE PATH FROM BASEDIR
+INVENTORY_JSON_PATH = BASEDIR / 'inventory.json'
+
+# INSTALLED/CREATED CLASSES FOR TYPES OF FOOD
+INSTALLED = {
+    "Meat":         ingredients.Meat,
+    "Vegetable":    ingredients.Vegetable,
+    "Dairy":        ingredients.Dairy,
+    "Fruit":        ingredients.Fruit,
+    "Flour":        ingredients.Flour,
+    "Seasoning":    ingredients.Seasoning,
+    "Other":        ingredients.Other
+}
